e2785c71f330623e63481d9915eb1daa20908611
Brian Lachniet <blachniet@gmail.com>
2013-05-07 14:38:56 -0400

    Edit view

---
+++ assets/javascripts/app/app-view.js
@@ -0,0 +1,9 @@
+define(['ember', 'templates'], function(Ember, templates) {
+
+  var ApplicationView;
+  return ApplicationView = Ember.View.extend({
+    name: 'Handlebars',
+    css: 'less'
+  });
+  
+});
---
+++ assets/javascripts/app/main.js
@@ -0,0 +1,7 @@
+
+define(['app/app-view'], function(ApplicationView) {
+  var App;
+  return App = {
+    ApplicationView: ApplicationView
+  };
+});
